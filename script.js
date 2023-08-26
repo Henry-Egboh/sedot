3a3c567b418aaa37b1527306e23f0517b3749919
Henry-Eg <107496624+Henry-Egboh@users.noreply.github.com>
2023-08-26 20:35:50 +0100

    implemented the categories menu using javascript and CSS animation

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const hamburger = document.querySelector(".hamburgerMenu");
+// ON MOBILE SCREENS
 const navMenu = document.querySelector(".navMenu");
+const hamburger = document.querySelector(".hamburgerMenu");
 const navLink = document.querySelectorAll(".navLink");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active");
 })
 
 navLink.forEach(link => link.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+// access the DOM using classname and store value in the menuBar variable
+const menu = document.querySelector(".menuBar");
+const listMenu = document.querySelector(".categoryListMenu");
+const h4 = document.querySelector(".categories-h4");
+
+// listen to click event on the accessed DOM
+menu.addEventListener("click", () => {
+    menu.classList.toggle("click");
+    listMenu.classList.toggle("click");
+    h4.classList.toggle("click");
+})
+
+
+
